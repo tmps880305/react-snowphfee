ac01c8def256c49b11fff40f0e92a20d200d1e40
tmps880305 <tmps880305@gmail.com>
2023-02-03 04:27:00 -0500

    Sketching ... 1. MainPage background image clipped (image temporary) 2. MainAbout background set 3. Fix some name typo error

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 import logoImage from '../../assets/img/logo.jpg';
 import classes from './Header.module.css';
@@ -7,15 +7,13 @@ import NavButton from './NavButton';
 
 const Header = (props) => {
     return (
-        <Fragment>
-            <header className={classes.header}>
-                <div className={classes.nav_container}>
-                    <img src={logoImage} alt="logo"/>
-                    <a href="/">jw1109@rit.edu</a>
-                    <NavButton/>
-                </div>
-            </header>
-        </Fragment>
+        <header className={classes.header}>
+            <div className={classes.nav_container}>
+                <img src={logoImage} alt="logo"/>
+                <a href="/">jw1109@rit.edu</a>
+                <NavButton/>
+            </div>
+        </header>
     )
 };
 
