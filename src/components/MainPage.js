40ec50d8880092fb58220a2a8a5635e083ab0190
tmps880305 <tmps880305@gmail.com>
2023-03-02 01:58:29 -0500

    Main page most done: 1. Add slogan

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import classes from './MainPage.module.css';
 import mainImage from '../assets/img/main_image.png'
-// import mainSlogan from '../assets/img/main_slogan.png'
 
 const MainPage = (props) => {
     return (
         <div className={classes.container}>
-            {/*<img src={mainSlogan} className={classes.mainSlogan} alt="main-slogan" />*/}
-            <h2 className={classes.slg}>Design, Communicate, Develop</h2>
-            <img src={mainImage} className={classes.mainImage} alt="main-background" />
+            <div className={classes.slg}>
+                <h2 className={classes.slg}>Design meets development - bridging the gap with multi-disciplinary
+                    experience</h2>
+            </div>
+            <img src={mainImage} className={classes.mainImage} alt="main-background"/>
         </div>
     )
 };
