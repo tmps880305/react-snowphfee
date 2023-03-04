d1f40e6adf08149b6526c9d0c024463ecb3c4941
tmps880305 <tmps880305@gmail.com>
2023-03-03 20:50:13 -0500

    Main page About RWD: 1. Change font-size to rem,vw with clamp 2. Sizing with vw and clamp 3. media query < 900, re-arrange items 4. media query < 600, background fill

--- src/components/MainAbout.js
+++ src/components/MainAbout.js
@@ -11,16 +11,16 @@ const MainAbout = (props) => {
                     <div className={classes['abt-line']}>
                         <span></span>
                     </div>
+                    <div className={classes['abt-title']}>
+                        <h3>Hi, I’m Jeni</h3>
+                        <h3>a ux developer</h3>
+                    </div>
                     <div className={classes['abt-selfie']}>
                         <img src={selfImage} alt="Selfie" />
                     </div>
                     <div className={classes['abt-info']}>
                         <p>I’m studying HCI at Rochester Institute of Technology. I enjoy develop various interest in my
                             free time.</p>
-                    </div>
-                    <div className={classes['abt-title']}>
-                        <h3>Hi, I’m Jeni</h3>
-                        <h3>a web developer</h3>
                     </div>
                     <div className={classes['abt-but']}>
                         <button>About me</button>
