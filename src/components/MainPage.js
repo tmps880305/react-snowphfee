f64bb141328a63f3cc06a6929496eac68855e29a
tmps880305 <tmps880305@gmail.com>
2023-03-03 19:40:44 -0500

    Main page 1st-page RWD: 1. Change font-size to rem,vw with clamp 2. Use only vw on shaping background image 3. media query when width < 1120px

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,8 +7,8 @@ const MainPage = (props) => {
     return (
         <div className={classes.container}>
             <div className={classes.slg}>
-                <h2 className={classes.slg}>Design meets development - bridging the gap with multi-disciplinary
-                    experience</h2>
+                <div className={classes.slg}>Design meets development - bridging the gap with multi-disciplinary
+                    experience</div>
             </div>
             <img src={mainImage} className={classes.mainImage} alt="main-background"/>
         </div>
