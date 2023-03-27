c81f13830f9575c49074fb64fe256e8c47852828
tmps880305 <tmps880305@gmail.com>
2023-03-26 23:42:02 -0400

    Fix bugs

--- src/components/Layout/Header/NavButton.js
+++ src/components/Layout/Header/NavButton.js
@@ -7,49 +7,29 @@ const NavButton = () => {
 
     // const DUMMY_DROP_LISTS = [
     //     {
-    //         id: 1,
     //         item: 'Front-end',
     //         link: 'skills'
     //     },
     //     {
-    //         id: 2,
     //         item: 'UX design',
     //         link: 'skills'
     //     },
     //     {
-    //         id: 3,
     //         item: 'Prototyping',
     //         link: 'skills'
     //     },
     //     {
-    //         id: 4,
     //         item: 'Accessibility',
     //         link: 'skills'
     //     }
     // ];
 
     const DUMMY_DROP_LISTS = [
-        {
-            id: 1,
-            item: 'Catch Cursor',
-            link: 'skills/catchcursor'
-        },
-        {
-            id: 2,
-            item: 'Pixel Paper',
-            link: 'skills/pixelpaper'
-        },
-        {
-            id: 3,
-            item: 'React - Portfolio',
-            link: 'skills'
-        },
-        // {
-        //     id: 4,
-        //     item: 'Accessibility',
-        //     link: 'skills'
-        // }
+        {item: 'Catch Cursor', link: 'skills/catchcursor'},
+        {item: 'Pixel Paper', link: 'skills/pixelpaper'},
+        {item: 'React - Portfolio', link: 'skills'},
     ];
+
 
     const resumeLink = 'https://drive.google.com/file/d/1WGzdJTH2XVJ2jpC0J0nBBxejS8alp6Md/view?usp=share_link';
 
@@ -60,8 +40,8 @@ const NavButton = () => {
                     <div className={classes.dropWrap}>
                         <NavLink to="skills">Skills</NavLink>
                         <ul className={classes['dropdown-content']}>
-                            {DUMMY_DROP_LISTS.map(item => {
-                                return <li key={item.id}><NavLink to={item.link}
+                            {DUMMY_DROP_LISTS.map((item,index) => {
+                                return <li key={index}><NavLink to={item.link}
                                                             className={classes.dropItem}>{item.item}</NavLink></li>
                             })}
                         </ul>
